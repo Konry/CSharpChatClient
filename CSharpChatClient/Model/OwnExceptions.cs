9039a60ec2da2731fb3cf24ec3af48c00b0bb76b
unknown <Konry@Egnatop.PORTFLY.local>
2016-01-02 03:06:33 +0100

    Moved Exception

---
+++ CSharpChatClient/Model/OwnExceptions.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace CSharpChatClient
+{
+    internal class ClientException : Exception
+    {
+        public ClientException(string message) : base(message)
+        {
+        }
+
+        public ClientException(string message, Exception inner)
+        : base(message, inner)
+        {
+        }
+    }
+
+    public class PortIsNotFreeException : Exception
+    {
+        public PortIsNotFreeException(string message) : base(message)
+        {
+        }
+
+        public PortIsNotFreeException(string message, Exception inner)
+    : base(message, inner)
+        {
+        }
+    }
+
+    public class AlreadyConnectedException : Exception
+    {
+        public AlreadyConnectedException(string message) : base(message)
+        {
+        }
+
+        public AlreadyConnectedException(string message, Exception inner)
+    : base(message, inner)
+        {
+        }
+    }
+}
